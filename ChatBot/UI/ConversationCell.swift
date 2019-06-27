52f979f2b222f8fcbdf4fc02e3dec7fa31e3c7d3
murad <muradkv@gmail.com>
2019-06-27 23:47:05 +0300

    Add UI: tableViewController with 3 custom cell

---
+++ ChatBot/UI/ConversationCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationCell.swift
+//  ChatBot
+//
+//  Created by murad on 27/06/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class ConversationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
