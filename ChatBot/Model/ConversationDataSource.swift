11cd823f78dfe89d2a2624928e1918ac962b38a1
murad <muradkv@gmail.com>
2019-07-03 23:49:18 +0300

    Fixing the message count and add question - answer conversation

--- ChatBot/Model/ConversationDataSource.swift
+++ ChatBot/Model/ConversationDataSource.swift
@@ -11,21 +11,28 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount = 0
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        messageCount += 1
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        messageCount += 1
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        
+        if index % 2 == 0 {
+            return Message(date: Date(), text: "Question \(index / 2)", type: .question)
+        } else {
+            return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
+        }
     }
 }
